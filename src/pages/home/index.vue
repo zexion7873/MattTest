9a191358b94ece480e25dd532f96ae9977af3904
MattLiu <sarmiley@gmail.com>
2020-05-25 10:37:56 +0800

    test router

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="home">
     <h1>測試是否有成功部署</h1>
+    <router-link :to="{name: 'NPS_EN'}">
+      <p>NPS EN</p>
+    </router-link>
+     <router-link :to="{name: 'NPS_TW'}">
+      <p>NPS TW</p>
+    </router-link>
   </div>
 </template>
 
